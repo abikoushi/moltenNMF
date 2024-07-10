8a71126ee354fcb0be87325c39eba898c3fdffe1
abikoushi <a.b.e.k.o.69@gmail.com>
2024-07-10 12:39:56 +0900

    binary dummy encoding

--- R/modelMatrix.R
+++ R/modelMatrix.R
@@ -1,15 +1,20 @@
-sparse_cate <- function(x,repr="C"){
+sparse_cate <- function(x, repr="C", binary_dummy=FALSE){
   if(!is.factor(x)){
     x <- factor(x)
     warning("auto-converted to factor")
   }
-  xi <- as.integer(x)
-  xi <- xi[!is.na(x)]
-  xp <- seq(1,length(x))
-  xp <- xp[!is.na(x)]
-  #val <- rep(TRUE, length(xi))
+  if(binary_dummy & length(levels(x)) == 2L){
+    xi <- as.integer(x)-1L
+    f <- xi > 0L & !is.na(x)
+    xi <- xi[f]
+    xp <- seq(1,length(xi))
+  }else{
+    xi <- as.integer(x)
+    xi <- xi[!is.na(x)]
+    xp <- seq(1,length(x))
+    xp <- xp[!is.na(x)]    
+  }
   m <- Matrix::sparseMatrix(i = xi, j = xp,
-                            #x = val,
                             dims = c(length(levels(x)), length(x)),
                             repr=repr)
   rownames(m) <- levels(x)
@@ -24,7 +29,8 @@ sparse_onehot <- function(object,
                           sep = "_",
                           interaction_operator = ":",
                           na.action='na.pass',
-                          repr = "C"){
+                          repr = "C",
+                          binary_dummy=FALSE){
   data <- model.frame(object, data, xlev=xlev, na.action=na.action)
   t <- if(missing(data)) terms(object) else terms(object, data=data)
   labs <- attr(t, "term.labels")
@@ -36,9 +42,9 @@ sparse_onehot <- function(object,
         ifelse(is.na(data[[x]]),NA_character_,as.character(data[[x]]))
         })
       ct <- ifelse(apply(is.na(cmat),1,any), NA_character_, apply(cmat, 1, paste, collapse=":"))
-      lx[[i]] <- sparse_cate(ct, repr = repr)
+      lx[[i]] <- sparse_cate(ct, repr = repr, binary_dummy = binary_dummy)
     }else{
-      lx[[i]] <- sparse_cate(data[[labs[i]]], repr = repr)      
+      lx[[i]] <- sparse_cate(data[[labs[i]]], repr = repr, binary_dummy = binary_dummy)      
     }
   }
   #if(dummy_m>=1L){
