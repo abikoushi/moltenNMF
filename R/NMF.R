aa1a090ac5f6d7fe5b7316838a86110b1038df6d
abikoushi <a.b.e.k.o.69@gmail.com>
2025-03-19 14:43:22 +0900

    2DNMF SVB with NA

--- R/NMF.R
+++ R/NMF.R
@@ -12,35 +12,48 @@ NMF2D_vb <- function(Y, rank,
     dims <- dim(Y)
   }
   if(is.null(Vini)){
-  # Vini <- list(t(apply(Y, 1, sample, size=rank)+0.1),
-  #               t(apply(Y, 2, sample, size=rank)+0.1))
-  # Vini <- list(matrix(sample(Y@x, size=dims[1]*rank, replace = TRUE), dims[1], rank),
-  #              matrix(sample(Y@x, size=dims[2]*rank, replace = TRUE), dims[2], rank))
   Vini <- list(matrix(rgamma(dims[1]*rank, 1, 10), dims[1], rank),
                  t(apply(Y, 2, sample, size=rank)+0.1))
   }
-  doVB_pois_2D(Vini,
-               y = Y@x, rowi = Y@i,  coli = Y@j,
-               dims = dims,
-               L = rank, iter=iter, 
-               a = prior_shape, b = prior_rate,
-               display_progress = display_progress)
+  naY = is.na(Y)
+  if(any(naY)){
+    weight <- list( (Y@Dim[1] - rowSums(naY))/Y@Dim[1],
+                    (Y@Dim[2] - colSums(naY))/Y@Dim[2] )
+    nn_wch = which(!is.na(Y@x))
+    out = doVB_pois_2D_ww(Vini,
+                       y = Y@x[nn_wch],
+                       rowi = Y@i[nn_wch],  coli = Y@j[nn_wch],
+                       dims = dims,
+                       L = rank, iter=iter, 
+                       weight = weight,
+                       a = prior_shape, b = prior_rate,
+                       display_progress = display_progress)
+  }else{
+    out = doVB_pois_2D(Vini,
+                       y = Y@x, rowi = Y@i,  coli = Y@j,
+                       dims = dims,
+                       L = rank, iter=iter, 
+                       a = prior_shape, b = prior_rate,
+                       display_progress = display_progress)
+  
+  }
+  return(out)
 }
 
 
 NMF2D_svb <- function(Y, rank,
-                         n_epochs, 
-                         n_baches,
-                         Vini = NULL,
-                         lr_param = c(1, 0.8),
-                         lr_type = "power",
-                         dims=NULL,
-                         weight = NULL,
-                         subiter = 1,
-                         prior_shape=1, prior_rate=1,
-                         index_decrement = 0,
-                         display_progress=TRUE,
-                         useonlyone=FALSE){
+                      n_epochs, 
+                      n_baches,
+                      Vini = NULL,
+                      lr_param = c(1, 0.8),
+                      lr_type = "power",
+                      dims=NULL,
+                      weight = NULL,
+                      subiter = 1,
+                      prior_shape=1, prior_rate=1,
+                      index_decrement = 0,
+                      display_progress=TRUE,
+                      useonlyone=FALSE){
   if(all(class(Y)!="dgTMatrix")){
     Y = as(Y, "TsparseMatrix")    
   }
--- example/testsim.R
+++ example/testsim.R
@@ -13,10 +13,26 @@ set_data_mf <- function(L, nrow, ncol, mu=0){
   list(Y=Y, trueW=W, trueH=H)
 }
 
-
 dat <- set_data_mf(2,110,100)
-out <- moltenNMF:::NMF2D_vb(dat$Y, rank = 2, iter = 500)
+
+library(Matrix)
+Y=dat$Y
+ind_na = sample.int(110*100, 1000)
+Y[ind_na] <- NA
+
+out <- moltenNMF:::NMF2D_vb(Y, rank = 2, iter = 500)
 plot(out$ELBO, type="l")
+V = moltenNMF:::meanV_2D(out)
+plot(log1p(V[[1]]%*%t(V[[2]])), as.matrix(log1p(dat$Y)), pch=1, cex=0.5, col=rgb(0,0,0,0.2))
+abline(0, 1, col="royalblue",lty=2)
+
+fit = V[[1]]%*%t(V[[2]])
+Yimp = Y
+plot(dat$Y[ind_na], fit[ind_na])
+abline(0,1)
+image(as.matrix(log1p(Y)), col=hcl.colors(12))
+
+
 lr = 10/nnzero(dat$Y)
 out <- moltenNMF:::NMF2D_svb(dat$Y, rank = 2,
                              n_epochs = 250, n_baches = 10,
@@ -77,3 +93,4 @@ Hhat_d <- logV[111:210,2:1]
 Hhat_d <- sweep(Hhat_d,1,rowMeans(Hhat_d))
 points(dat$trueH,t(Hhat_d), col=rgb(1,0,0,0.2))
 abline(0,1,lty=2)
+
