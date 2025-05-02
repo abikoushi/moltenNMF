202d78ec42ef46a49ef68ef8bce408ea7df1ec20
abikoushi <a.b.e.k.o.69@gmail.com>
2025-05-02 18:28:17 +0900

    run svb

--- example/sim_vsLiger.R
+++ example/sim_vsLiger.R
@@ -1,9 +1,11 @@
 #install.packages('RcppPlanc', repos = c('https://welch-lab.r-universe.dev', 'https://cloud.r-project.org'))
+#devtools::install_github("welch-lab/liger")
+#devtools::install_github('welch-lab/RcppPlanc')
 library(rliger)
 library(Matrix)
 library(moltenNMF)
 library(dplyr)
-####
+
 maxcor <- function(A, B){
   cor_0 <- cor(A,B)
   apply(cor_0, 1, max)
@@ -71,16 +73,16 @@ system.time2 <- function(expr){
   t0$mem_used = mem_after - mem_before
   return(t0)
 }
-#####
+
 set_simpar <- function(nrow, ncol, L, logmu=0){
   V1 <- matrix(rlnorm(L*nrow, meanlog = 0, sdlog = seq(1, 0.1, length.out=L)), nrow = nrow, ncol = L, byrow = TRUE)
-  V2 <- matrix(rlnorm(L*ncol, logmu, 1), nrow = L, ncol = ncol)
+  V2 <- matrix(rlnorm(L*ncol, logmu, sdlog = 1), nrow = L, ncol = ncol)
+  hist(V2)
   ord = order(apply(V1, 2, var), decreasing=TRUE)
   V1 = V1[,ord]
   V2 = V2[ord,]
   list(mu=V1%*%V2, V1=V1, V2=V2)
 }
-
 
 samplemat_norm <-function(tpar, obs_sigma){
   with(tpar,
@@ -94,15 +96,12 @@ samplemat_pois <-function(tpar){
   )
 }
 
-####
-
 logmu = c(-5, -2, 0)
 n_rows <- c(1000, 2000, 5000)
 n_cols <- c(2000)
 L_rank <- c(10L, 15L, 30L)
 repl <- 10 
 df <- expand.grid(n_rows=n_rows,n_cols=n_cols, logmu=logmu, L_rank=L_rank)
-dim(df)
 
 res_bm <- vector("list", nrow(df))
 res_cor <- vector("list", nrow(df))
@@ -114,13 +113,13 @@ for(i in 1:nrow(df)){
   nrow = df$n_rows[i]
   ncol = df$n_cols[i]
   maxit=1000
-  tpar <- set_simpar(nrow, ncol, L, logmu = logmu[i])
-  #hist(tpar$mu, breaks = "FD")
-  #sp = with(tpar, mean(zeroprob_norm(mu, obs_sigma)))
+  tpar <- set_simpar(nrow, ncol, L, logmu = df$logmu[i])
+  tpar$V2[1:5,1:5]
   sp = with(tpar, mean(exp(-tpar$mu)))
   sparsity[i] <- sp
   #Z = samplemat_norm(tpar, obs_sigma = 1)
   Z = samplemat_pois(tpar)
+  hist(tpar$mu)
   Z = as(Z, "TsparseMatrix")
   rank = L
   t0 <- system.time2({
@@ -130,8 +129,8 @@ for(i in 1:nrow(df)){
                                     display_progress = FALSE)
   })
   
-
   t3 <- system.time2({
+    hist(as.matrix(Z))
     rownames(Z) <- paste0("gene", 1:nrow(Z))
     colnames(Z) <- paste0("cell", 1:ncol(Z))
     liger_obj = createLiger( list(sample1=Z) )
@@ -146,11 +145,8 @@ for(i in 1:nrow(df)){
   
   Vhat_t <- moltenNMF:::meanV_array(decomp0)
   Vhat_t <- moltenNMF:::rearrange_cols(Vhat_t, FUN = var, normalize = FALSE, decreasing = TRUE)
-  
   fit_0 <- Vhat_t[[1]]%*%t(Vhat_t[[2]])
   fit_3 <- fit_liger(liger_obj)
-  
-  
   Vhat_3 <- list(extract_row_liger(liger_obj), t(getMatrix(liger_obj, slot = "H")[[1]]))
   Vhat_3 <- moltenNMF:::rearrange_cols(Vhat_3, FUN = var, normalize = FALSE, decreasing = TRUE)
   
@@ -165,16 +161,15 @@ for(i in 1:nrow(df)){
               rmse = rmse(as.matrix(Z), fit_3),
               rmse_t = rmse(tpar$mu, fit_3), 
               method="rliger")
-  
+
   bm = rbind(t0,t3)
   
-  rho <- cbind(diagcor(Vhat_t[[1]], tpar$V1),diagcor(Vhat_3[[1]], tpar$V1))
+  rho <- cbind(diagcor(Vhat_t[[1]], tpar$V1), diagcor(Vhat_3[[1]], tpar$V1))
   
   res_bm[[i]] <- bm
   res_cor[[i]] <- rho
   setTxtProgressBar(pb,i)
 }
-
 
 save(res_bm, res_cor, sparsity, file = "vsliger_minibatch.Rdata")
 
